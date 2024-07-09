43f2fd4c930d0bb54d99fa56377929240de382b1
Vladislav Yakubenko <vladislavy10@gmail.com>
2024-07-09 21:46:10 +0300

    Homework_04 (#5)
    * 'Homework_04'
    
    * 'Homework_04'
    
    * 'Homework_04'
    
    * update ;
    
    * Update package.json
    
    ---------
    
    Co-authored-by: Vladislav Yakubenko <vladislav.yakubenko@oxagile.com>

---
+++ Homework_04/hw.js
@@ -0,0 +1,23 @@
+const fse = require("fs-extra");
+
+const file = '/Homework_04/Folder_01/hw_04.txt';
+
+fse.ensureFileSync (file);
+
+const dir2 = '/Homework_04/Folder_02';
+
+fse.ensureDirSync(dir2);
+
+fse.moveSync('/Homework_04/Folder_01/hw_04.txt','/Homework_04/Folder_02/hw_04.txt');
+
+const dir3 = '/Homework_04/Folder_03';
+
+fse.ensureDirSync(dir3);
+
+fse.copySync('/Homework_04/Folder_02/hw_04.txt', '/Homework_04/Folder_03/hw_04.txt');
+
+fse.removeSync('/Homework_04/Folder_01');
+
+fse.removeSync('/Homework_04/Folder_02');
+
+fse.removeSync('/Homework_04/Folder_03');
