190e20c8957244a132aa94aad579f03403dd064e
Vladislav Yakubenko <vladislav.yakubenko@oxagile.com>
2024-07-11 21:26:24 +0300

    update temp files

---
+++ Temp/number.js
@@ -0,0 +1,43 @@
+console.log (10 + 10)
+console.log (10 - 10)
+console.log (10 / 10)
+console.log (10 * 10)
+console.log (8 % 3)
+
+console.log (Math.PI)
+
+console.log (Math.max (2, 4, 6, 737,735773,747,7377))
+
+
+console.log (Math.floor((Math.random ()*(100-1) + 1)))
+
+console.log (Math.floor(10.9))
+
+console.log (Math.ceil(10.1))
+
+console.log (Math.round(10.5))
+
+console.log (0 == false);
+
+console.log (0 == "");
+
+console.log (0 != "");
+
+console.log ("" == false);
+
+console.log (0 === false);
+
+console.log (0 !== false);
+
+console.log (10 > 9);
+
+console.log (10 >= 9);
+
+console.log (10 <= 9);
+
+console.log (10 < 9);
+
+
+
+
+
---
+++ Temp/path/path.js
@@ -0,0 +1,6 @@
+const path = require ('path')
+
+
+console.log('dirname --- ', __dirname)
+console.log('filename --- ', __filename)
+console.log('process --- ', process.cwd())
