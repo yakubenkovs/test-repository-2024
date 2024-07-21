10527d2aab91abe2dc0b7eac355f56b2dfc38582
Vladislav Yakubenko <vladislavy10@gmail.com>
2024-07-21 15:32:23 +0300

    Update hw_08 with task 2.js

--- Homework_08/hw_08.js
+++ Homework_08/hw_08.js
@@ -1,6 +1,8 @@
+let numberForSplitting = `462822`;
+
 function checkPartsEqualityOfTheNumber (numberForSplitting) {
     const digitsForComparison = numberForSplitting.split('');
-    let firstPartOfTheNumber = [], secondPartOfTheNumber = [], summOfTheFirstDigitsPart = 0, summOfTheSecondDigitsPart = 0;
+    let firstPartOfTheNumber = [], secondPartOfTheNumber = [], summOfTheFirstDigitsPart = 0, summOfTheSecondDigitsPart = 0, isPartsOfTheNumberEqual;
     secondPartOfTheNumber = digitsForComparison.slice(digitsForComparison.length / 2);
     firstPartOfTheNumber = digitsForComparison.splice(0, digitsForComparison.length / 2);
 
@@ -14,6 +16,21 @@ function checkPartsEqualityOfTheNumber (numberForSplitting) {
     return isPartsOfTheNumberEqual;
 }
 
-let numberForSplitting = `462822`, isPartsOfTheNumberEqual;
-checkPartsEqualityOfTheNumber(numberForSplitting);
-isPartsOfTheNumberEqual ? console.log(`Yes. Sums of parts of the number ${numberForSplitting} are equal`) : console.log(`No. Sums of parts of the number ${numberForSplitting} aren't equal`);
+checkPartsEqualityOfTheNumber(numberForSplitting) ? console.log(`Yes. Sums of parts of the number ${numberForSplitting} are equal`) : console.log(`No. Sums of parts of the number ${numberForSplitting} aren't equal`);
+
+
+let numberForDivision = 500, limitNumber = 15;
+
+function divideNumberBy2 (numberForDivision, limitNumber) {
+    let num = 0;
+
+    for (numberForDivision; numberForDivision > limitNumber; numberForDivision /= 2) {
+        num++;
+    }
+    
+    numberForDivision *= 2;
+    num--;
+    return {numberForDivision, num};
+}
+
+console.log(`Finish number after division ${numberForDivision} by 2 before limit of ${limitNumber} is ${divideNumberBy2 (numberForDivision, limitNumber).numberForDivision} and iterations number is ${divideNumberBy2 (numberForDivision, limitNumber).num}`);
