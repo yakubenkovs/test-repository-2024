75b01e0a5ab951f8f1512644121e81a3d15d51f7
Vladislav Yakubenko <vladislav.yakubenko@oxagile.com>
2024-07-24 01:12:53 +0300

    update hw_09

--- Homework_09/hw_09.js
+++ Homework_09/hw_09.js
@@ -7,3 +7,16 @@ const promiseTwo = new Promise(resolve => setTimeout(resolve, randomValue(), '2'
 const promiseThree = new Promise(resolve => setTimeout(resolve, randomValue(), '3'));
 
 Promise.race([promiseOne, promiseTwo, promiseThree]).then(value => console.log(value));
+
+
+function getNum (min = 1, max = 5, timeoutValue = 3000) {
+    return promise = new Promise(resolve => setTimeout(resolve, timeoutValue, Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + Math.ceil(min))));
+}
+
+async function squareTheNumber() {
+    let squaredNumber = await getNum();
+    console.log (`Value ${squaredNumber} squared is ${Math.pow(squaredNumber,2)}`);
+    return Math.pow(squaredNumber,2);
+}
+
+console.log(squareTheNumber());
