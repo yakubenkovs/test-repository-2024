cc00f0f4aac59f84bac0e24b20b6df9f8dc197ec
Vladislav Yakubenko <vladislav.yakubenko@oxagile.com>
2024-07-23 23:56:33 +0300

    update hw_09

--- Homework_09/hw_09.js
+++ Homework_09/hw_09.js
@@ -1,9 +1,3 @@
-// const promiseOne = new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * (Math.floor(5) - Math.ceil(1) + 1) + Math.ceil(1), 'promiseOne')));
-// const promiseTwo = new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * (Math.floor(5) - Math.ceil(1) + 1) + Math.ceil(1), 'promiseTwo')));
-// const promiseThree = new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * (Math.floor(5) - Math.ceil(1) + 1) + Math.ceil(1), 'promiseThree')));
-
-// Promise.race([promiseOne, promiseTwo, promiseThree]).then(value => console.log(value));
-
 randomValue = Math.floor(Math.random() * (Math.floor(5) - Math.ceil(1) + 1) + Math.ceil(1));
 
 const promise1 = new Promise(resolve => setTimeout(resolve, randomValue, '1'));
