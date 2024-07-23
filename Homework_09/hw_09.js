ea794f5e0ae36eb42cc4100c0f696cb89283cab5
Vladislav Yakubenko <vladislav.yakubenko@oxagile.com>
2024-07-23 23:55:39 +0300

    update hw_09

--- Homework_09/hw_09.js
+++ Homework_09/hw_09.js
@@ -1,5 +1,13 @@
-const promiseOne = new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * (Math.floor(5) - Math.ceil(1) + 1) + Math.ceil(1), 'promiseOne')));
-const promiseTwo = new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * (Math.floor(5) - Math.ceil(1) + 1) + Math.ceil(1), 'promiseTwo')));
-const promiseThree = new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * (Math.floor(5) - Math.ceil(1) + 1) + Math.ceil(1), 'promiseThree')));
+// const promiseOne = new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * (Math.floor(5) - Math.ceil(1) + 1) + Math.ceil(1), 'promiseOne')));
+// const promiseTwo = new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * (Math.floor(5) - Math.ceil(1) + 1) + Math.ceil(1), 'promiseTwo')));
+// const promiseThree = new Promise(resolve => setTimeout(resolve, Math.floor(Math.random() * (Math.floor(5) - Math.ceil(1) + 1) + Math.ceil(1), 'promiseThree')));
 
-Promise.race([promiseOne, promiseTwo, promiseThree]).then(value => console.log(value));
+// Promise.race([promiseOne, promiseTwo, promiseThree]).then(value => console.log(value));
+
+randomValue = Math.floor(Math.random() * (Math.floor(5) - Math.ceil(1) + 1) + Math.ceil(1));
+
+const promise1 = new Promise(resolve => setTimeout(resolve, randomValue, '1'));
+const promise2 = new Promise(resolve => setTimeout(resolve, randomValue, '2'));
+const promise3 = new Promise(resolve => setTimeout(resolve, randomValue, '3'));
+
+Promise.race([promise1, promise2, promise3]).then(value => console.log(value));
