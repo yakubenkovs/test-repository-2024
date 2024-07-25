15aaedf9bc0ff28d6b0c44eebf1f12834d93e1fb
Vladislav Yakubenko <vladislav.yakubenko@oxagile.com>
2024-07-25 21:39:41 +0300

    update hw_09

--- Homework_09/hw_09.js
+++ Homework_09/hw_09.js
@@ -2,21 +2,23 @@ function randomValue(min = 1, max = 5) {
     return Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + Math.ceil(min));
 }
 
-const promiseOne = new Promise(resolve => setTimeout(resolve, randomValue(), '1'));
-const promiseTwo = new Promise(resolve => setTimeout(resolve, randomValue(), '2'));
-const promiseThree = new Promise(resolve => setTimeout(resolve, randomValue(), '3'));
+function getPromise(screenValue) {
+    return new Promise (resolve => setTimeout(resolve, randomValue(), screenValue));
+}
 
-Promise.race([promiseOne, promiseTwo, promiseThree]).then(value => console.log(value));
+const promiseOne = getPromise(1), promiseTwo = getPromise(2), promiseThree = getPromise(3);
+
+Promise.race([promiseOne, promiseTwo, promiseThree]).then(firstValueByTime => console.log(`First promise is ${firstValueByTime}`));
 
 
 function getNum (min = 1, max = 5, timeoutValue = 3000) {
-    return promise = new Promise(resolve => setTimeout(resolve, timeoutValue, Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + Math.ceil(min))));
+    return new Promise(resolve => setTimeout(resolve, timeoutValue, Math.floor(Math.random() * (Math.floor(max) - Math.ceil(min) + 1) + Math.ceil(min))));
 }
 
 async function squareTheNumber() {
-    let squaredNumber = await getNum();
-    console.log (`Value ${squaredNumber} squared is ${Math.pow(squaredNumber,2)}`);
-    return Math.pow(squaredNumber,2);
+    const numberValue = await getNum();
+    let squaredNumber = Math.pow(numberValue, 2);
+    return {numberValue: numberValue, squaredNumber: squaredNumber};
 }
 
-console.log(squareTheNumber());
+console.log(squareTheNumber().then(squaredValues => console.log(`Value ${squaredValues.numberValue} squared is ${squaredValues.squaredNumber}`)));
