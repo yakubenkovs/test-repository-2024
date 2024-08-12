84d04dbbb4571c794dbfc77fdc03fa93faaeb11a
Vladislav Yakubenko <vladislav.yakubenko@oxagile.com>
2024-08-12 23:01:29 +0300

    update Homework_12

--- Homework_12/hw_12.ts
+++ Homework_12/hw_12.ts
@@ -114,3 +114,18 @@ export function filter<filtererType, inputType>(filterer?: (value: inputType) =>
     }
     return input.filter(filterer);
 }
+
+export function add(a?: number, b?: number): number | Function {
+    if (arguments.length === 0) {
+        return add;
+    }
+    if (arguments.length === 1) {
+        return function subFunction(subB: number): number | Function {
+            if (arguments.length === 0) {
+                return subFunction;
+            }
+            return a + subB;
+        };
+    }
+    return a + b;
+}
