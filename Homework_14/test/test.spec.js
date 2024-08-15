9d5299a96b7826c6f255364a836916b707fbea12
Vladislav Yakubenko <vladislav.yakubenko@oxagile.com>
2024-08-15 23:42:33 +0300

    Homework 14

--- Homework_14/test/test.spec.js
+++ Homework_14/test/test.spec.js
@@ -5,6 +5,6 @@ describe('Add function check', () => {
     it('Check of correct addition of two positive numbers', async () => {
         const calculator = new Calculator();
         const result = calculator.add(15,5);
-        expect(result).to.equal(19);
+        expect(result).to.equal(20);
     })
 });
